baae1dcdd58451f66cd8a52c803cebb0c6350e4d
=Renedix <damiensebay88@googlemail.com>
2017-07-01 11:25:10 -0400

    TODOS

--- app/src/main/java/com/workout/renedix/renedixworkout/data/Pojo/Workout.java
+++ app/src/main/java/com/workout/renedix/renedixworkout/data/Pojo/Workout.java
@@ -1,5 +1,7 @@
 package com.workout.renedix.renedixworkout.data.Pojo;
 
+import java.util.List;
+
 /**
  * Created by RENEDIX-LAPTOP on 6/19/2017.
  */
@@ -8,6 +10,9 @@ public class Workout {
     public int id;
     public String name;
     public String description;
+    public List<Integer> cardioExerciseIds;
+    public List<Integer> resistanceExerciseIds;
+
 
     public Workout(String name, String description){
         this.name = name;
@@ -24,6 +29,17 @@ public class Workout {
         this.name = new String(workout.name);
         this.description = new String(workout.description);
     }
+
+
+
+    // TODO cardioExerciseIds
+    // TODO resistanceExerciseIds
+    // TODO Add dummy exercises to all workouts
+    // TODO view all exercises for workout on Workout form and Workout Detail (fragment?)
+    // TODO Allow users to add and remove cardio exercises
+    // TODO Allow users to add and remove resistance exercises
+
+
 
 
 }

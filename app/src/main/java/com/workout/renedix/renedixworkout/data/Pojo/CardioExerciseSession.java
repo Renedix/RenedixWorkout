556d96d8b928053432749490807d3a45c59d0a05
=Renedix <damiensebay88@googlemail.com>
2017-07-05 20:52:08 -0400

    Pojos now have fields

--- app/src/main/java/com/workout/renedix/renedixworkout/data/Pojo/CardioExerciseSession.java
+++ app/src/main/java/com/workout/renedix/renedixworkout/data/Pojo/CardioExerciseSession.java
@@ -1,18 +1,40 @@
 package com.workout.renedix.renedixworkout.data.Pojo;
 
+import java.util.Date;
+
 /**
  * Created by RENEDIX-LAPTOP on 7/5/2017.
  */
 
 public class CardioExerciseSession {
-    private CardioExercise exercise;
 
-    /*  Cardio
-    *   -> Duration
-    *   -> Distance
-    *   -> Resistance
-    *   -> Incline
-    *   -> Calories
-    * */
+    public int id;
+    public CardioExercise exercise;
+    public Duration duration;
+    public Distance distance;
+    public double resistance;
+    public double incline;
+    public int calories;
+    public Date logDate;
+
+    public enum DistanceUnit {MILES,KILOMETER};
+    private class Distance{
+        public DistanceUnit unit;
+        public float amount;
+        public Distance(float amount, DistanceUnit unit){
+            this.unit = unit;
+            this.amount = amount;
+        }
+    }
+    private class Duration{
+        private int seconds;
+        private int minutes;
+        private int hours;
+        public Duration(int seconds, int minutes, int hours){
+            this.seconds = seconds;
+            this.minutes = minutes;
+            this.hours = hours;
+        }
+    }
 
 }
--- app/src/main/java/com/workout/renedix/renedixworkout/data/Pojo/ResistanceExerciseSession.java
+++ app/src/main/java/com/workout/renedix/renedixworkout/data/Pojo/ResistanceExerciseSession.java
@@ -1,11 +1,18 @@
 package com.workout.renedix.renedixworkout.data.Pojo;
 
+import java.util.Date;
+
 /**
  * Created by RENEDIX-LAPTOP on 7/5/2017.
  */
 
 public class ResistanceExerciseSession {
-    private ResistanceExercise exercise;
+
+    public int id;
+    public ResistanceExercise exercise;
+    public int repetitions;
+    public double resistance;
+    public Date logDate;
 
     /*  Resistance
     *   -> Repetitions
